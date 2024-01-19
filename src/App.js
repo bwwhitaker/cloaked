17f048663140f4f011f66010b4a66aa0b26cb7d3
Brian Whitaker <43477669+bwwhitaker@users.noreply.github.com>
2024-01-18 16:48:42 -0800

    Add Logo and Block Mobile
    Add a logo courtesy of '<a href="https://www.flaticon.com/free-icons/ufo" title="ufo icons">Ufo icons created by 3ab2ou - Flaticon</a>' and block mobile access to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import './App.css';
 import GameSpace from './Components/GameSpace';
+import React, { useEffect, useState } from 'react';
+
+const MobileErrorMessage = () => {
+	return (
+		<div>
+			<h1>Error: Mobile Devices Not Supported</h1>
+			<p>This application is not supported on mobile devices. Please use a desktop or laptop to access the content.</p>
+		</div>
+	);
+};
 
 function App() {
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		const checkIfMobile = () => {
+			setIsMobile(/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)); // Adjust this threshold based on your needs
+		};
+
+		// Check on initial mount
+		checkIfMobile();
+
+		// Check on window resize
+		window.addEventListener('resize', checkIfMobile);
+
+		// Cleanup event listener on component unmount
+		return () => {
+			window.removeEventListener('resize', checkIfMobile);
+		};
+	}, []);
 	return (
 		<div className='App'>
-			<div className='App-header'>
-				<GameSpace />
-			</div>
+			<div className='App-header'>{isMobile ? <MobileErrorMessage /> : <GameSpace />}</div>
 		</div>
 	);
 }
