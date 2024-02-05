d75e3688ff5403ce3da1ce155731d01d44632c6c
Brian Whitaker <43477669+bwwhitaker@users.noreply.github.com>
2024-02-04 21:44:57 -0800

    Update Square.js
    Removed the Button and added styling to Paper

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -12,12 +12,13 @@ function Square(props) {
 		setScanDialogueOpen(!scanDialogueOpen);
 		props.handleScanCloakedShip();
 	};
+	const [currentFontColor, setCurrentFontColor] = useState('black');
 
 	const isInArray = (value, array) => {
 		return array.includes(value);
 	};
 
-	function ButtonClick(id) {
+	function SquareClick(id) {
 		const clickMode = props.clickMode;
 		let a = id - 1;
 		let b = id + 1;
@@ -40,12 +41,15 @@ function Square(props) {
 			props.updateTargeted(id);
 		} else if (clickMode === 'Unlock' && isInArray(id, targeted)) {
 			setCurrentBackgroundColor('white');
+			setCurrentFontColor('black');
 			props.removeTargeted(id);
 		} else if (clickMode === 'Scan' && isInArray(id, targeted)) {
 			setCurrentBackgroundColor('black');
+			setCurrentFontColor('white');
 			props.removeTargeted(id);
 		} else if (clickMode === 'Unlock') {
 			setCurrentBackgroundColor('white');
+			setCurrentFontColor('black');
 		} else if (isInArray(a, shipLocations) && id % axis !== 1) {
 			setCurrentBackgroundColor('blue');
 		} else if (isInArray(b, shipLocations) && id % axis !== 0) {
@@ -56,39 +60,37 @@ function Square(props) {
 			setCurrentBackgroundColor('blue');
 		} else {
 			setCurrentBackgroundColor('black');
+			setCurrentFontColor('white');
 		}
 	}
 
 	const PaperPlace = styled(Paper)(() => ({
 		textAlign: 'center',
+		fontSize: '15px',
 		height: 50,
 		width: 50,
 		lineHeight: '50px',
 		backgroundColor: currentBackgroundColor,
 		justifyContent: 'center',
-	}));
-
-	const ButtonPlace = styled(Button)(() => ({
-		maxWidth: '48px',
-		maxHeight: '48px',
-		minWidth: '48px',
-		minHeight: '48px',
-		padding: 0,
-		outlineColor: 'black',
-		backgroundColor: currentBackgroundColor,
+		transition: 'background-color 0.3s ease',
+		'&:hover': {
+			cursor: 'pointer',
+			color: 'blue',
+		},
+		color: currentFontColor,
 	}));
 
 	return (
 		<div>
-			<PaperPlace square variant='outlined'>
-				<ButtonPlace
-					key={props.title}
-					onClick={() => {
-						ButtonClick(props.title);
-					}}
-				>
-					{props.title}
-				</ButtonPlace>
+			<PaperPlace
+				square
+				variant='outlined'
+				key={props.title}
+				onClick={() => {
+					SquareClick(props.title);
+				}}
+			>
+				{props.title}
 			</PaperPlace>
 
 			<Snackbar open={scanDialogueOpen} onClose={handleClose} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
